04fe1ef7fe73073503ebc9ce00b59ef2468a617b
apenoob2k <thomas.stenhaug@hotmail.com>
2026-02-06 11:35:32 +0100

    rolls 10 Dice and displays the single rolls and the sum of them all

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// random dice rolls
+
+Console.WriteLine(" Rolling 1 D6 dice");
+//Random rng = new Random();
+//int Dice =rng.Next (1,21);
+//Console.WriteLine(Dice);
+int rolls =0;
+int sum =0;
+Console.WriteLine(sum);
+
+
+while(rolls < 10)
+{
+Random rng = new Random();
+int Dice =rng.Next (1,7);
+Console.WriteLine(Dice);
+sum += Dice;
+rolls ++;
+}
+
+Console.WriteLine("total Sum is " + sum);
